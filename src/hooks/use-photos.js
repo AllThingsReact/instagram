ef0c407626e54964568e8d1a7732f0aeaf80f957
AllThingsReact <>
2021-03-31 23:24:44 +0530

    Get photos to be displayed on timeline

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,29 @@
+import { useContext, useEffect, useState } from "react";
+import { getPhotos, getUserByUserId } from "../services/firebase";
+import UserContext from "../context/user";
+
+export default function usePhotos() {
+  const [photos, setPhotos] = useState(null);
+
+  const {
+    user: { uid: userId = "" },
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      const [{ following }] = await getUserByUserId(userId);
+      let followedUserPhotos = [];
+
+      if (following.length) {
+        followedUserPhotos = await getPhotos(userId, following);
+      }
+
+      followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+      setPhotos(followedUserPhotos);
+    }
+
+    getTimelinePhotos();
+  }, [userId]);
+
+  return { photos };
+}
