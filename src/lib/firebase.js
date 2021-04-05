c4e3d6c421b479626ff95fcb61271062e7c2351f
AllThingsReact <56463643+AllThingsReact@users.noreply.github.com>
2021-04-05 12:00:15 +0530

    Firebase lib update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,14 +2,7 @@ import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyAFl8gGlEFvNG7jj2gbd8eRX7m-KetFyN8",
-  authDomain: "instagram-87bb9.firebaseapp.com",
-  projectId: "instagram-87bb9",
-  storageBucket: "instagram-87bb9.appspot.com",
-  messagingSenderId: "616711316970",
-  appId: "1:616711316970:web:7747c5bb986ba539683e93",
-};
+const config = {};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
